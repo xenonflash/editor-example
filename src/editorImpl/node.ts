da2c103d4cd55acc7c234d40682dcc908e39108a
yankechao <yankechao@MacBook-Pro.local>
2021-05-12 19:01:40 +0800

    add more properties for node

--- src/editorImpl/node.ts
+++ src/editorImpl/node.ts
@@ -8,11 +8,18 @@ export interface NodeOptions {
     draggable?: boolean;
     resizable?: boolean;
     rotatable?: boolean;
+    top: number;
+    left: number;
+    rotate: number;
 }
 
 const defaultNodeOption: NodeOptions = {
     width: 100,
     height: 100,
+    top: 0,
+    left: 0,
+    left: 0,
+    rotate: 0,
     draggable: true,
     resizable: true,
     rotatable: true
@@ -28,24 +35,43 @@ export default class Node {
     public draggable: boolean
     public resizable: boolean
     public rotatable: boolean
+    public name = ''
     private frame = {
         translate: [0, 0],
         rotate: 0,
         transformOrigin: "50% 50%",
     };
+    // 尺寸信息
+    public width: number
+    public height: number
+    public anchorX: number
+    public anchorY: number
+    public rotate: number
+
     constructor(parent: Moveable, options = defaultNodeOption) {
-        this.draggable = options.draggable
-        this.resizable = options.resizable
-        this.rotatable = options.rotatable
+        Object.assign(defaultNodeOption, options)
+        const { draggable, resizable, rotatable, width, height, top, left, rotate } = options
+        this.draggable = draggable
+        this.resizable = resizable
+        this.rotatable = rotatable
         this.id = uuid()
         this.parent = parent
         this.parentEl = parent.el
         const el = document.createElement('div')
-        el.style.width = options.width + 'px'
-        el.style.height = options.height + 'px'
+        el.style.width = width + 'px'
+        el.style.height = height + 'px'
         el.style.background = '#eee'
         el.className = nodeClassName
+        el.id = this.id
+        el.style.transform = `rotate(${rotate})`
+        this.frame.rotate = rotate
         this.el = el
+        this.width = width
+        this.height = height
+        this.left = left
+        this.top = top
+        this.rotate = rotate
+        
         el.addEventListener('click', this.select.bind(this))
     }
     public select(): Node {
